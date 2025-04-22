7574511c20c9d63ff80987b619f9828c2b205ba9
salonisaboo <saloni0927@gmail.com>
2025-04-22 15:13:31 +0530

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+ //function mongodb database connection
+ const connectDb = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log(`Connected to Database ${mongoose.connection.host}`.bgCyan); 
+    }catch(error){
+        console.log('DB Error',error);
+    }
+ };
+module.exports=connectDb;
---
+++ routes/userRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { getUserController, updateUserController, updatePasswordController ,resetPasswordController,
+    deleteProfileController, } = require("../controllers/userController");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+const router = express.Router();
+//routes
+// GET USER||GEt
+router.get("/getUser",authMiddleware, getUserController);
+//UPDATE PROFILE
+router.put("/updateUser",authMiddleware,updateUserController);
+//password update
+router.post("/updatePassword",authMiddleware,updatePasswordController);
+// RESET PASSWORD
+router.post("/resetPassword", authMiddleware, resetPasswordController);
+
+// delete USER
+router.delete("/deleteUser/:id", authMiddleware, deleteProfileController);
+
+module.exports=router;
